febeaec95df35f3fd46ef6c4fb36886eff9df4e5
Ander <ander.uraga@gmail.com>
2016-10-26 09:19:43 +0200

    initial import

--- swift3FotoPicker/ViewController.swift
+++ swift3FotoPicker/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Photos
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var imagenFoto: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func imagenButtonClicked(_ sender: UIBarButtonItem) {
+        NSLog("Abrir selector de Fotos")
+        
+        //llamar controlador de ImagenPicker
+        let vc = UIImagePickerController()
+        vc.modalPresentationStyle = .formSheet  //estilo modal
+        vc.delegate = self                      //delegado nuestro ViewController
+        
+        //presentar controlador
+        navigationController?.present(vc, animated: true, completion: nil)
+   
+    }
+    
+    
+    @IBAction func saveButtonClicked(_ sender: UIBarButtonItem) {
+        NSLog("Guardar Imagen")
+    }
+    
+    
+    
+    
+    //MARK: UIImagePickerControllerDelegate
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let imagen = info[UIImagePickerControllerOriginalImage] as? UIImage{
+            self.imagenFoto.image = imagen
+        }
+        
+        //cerrar controlador
+        picker.dismiss(animated: true, completion: nil)
+    }
 
 }
-
