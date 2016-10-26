9c0550e66082bf341c455947de74017221ca7c75
Ander <ander.uraga@gmail.com>
2016-10-26 08:46:38 +0200

    Initial Commit

---
+++ swift3FotoPicker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift3FotoPicker
+//
+//  Created by Spot Matic SL on 26/10/16.
+//  Copyright © 2016 Spot Matic SL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
